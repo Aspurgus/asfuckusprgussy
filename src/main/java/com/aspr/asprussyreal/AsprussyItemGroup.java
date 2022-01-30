90117663d37b4df0b8d9257ddb87416815c2ea5e
Aspurgus <75339765+Aspurgus@users.noreply.github.com>
2022-01-30 11:07:05 -0500

    Plans and stuf !

---
+++ src/main/java/com/aspr/asprussyreal/AsprussyItemGroup.java
@@ -0,0 +1,4 @@
+package com.aspr.asprussyreal;
+
+public class AsprussyItemGroup {
+}
--- src/main/java/com/aspr/asprussyreal/AsprussyReal.java
+++ src/main/java/com/aspr/asprussyreal/AsprussyReal.java
@@ -14,6 +14,7 @@ public class AsprussyReal
 
     public AsprussyReal() {
 
+        
 
         MinecraftForge.EVENT_BUS.register(this);
 
